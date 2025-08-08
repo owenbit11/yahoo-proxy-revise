279eae96ae3a0b944a99a8fd1b128dbd78cf49ba
owenbit11 <l819596262@gmail.com>
2025-08-08 18:34:39 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,3 +1,6 @@
+Access-Control-Allow-Origin: *
+Access-Control-Allow-Methods: GET,OPTIONS
+Access-Control-Allow-Headers: Content-Type
 
 export default async function handler(req, res) {
   const symbol = req.query.symbol;
